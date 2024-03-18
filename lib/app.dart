34a15e30f048500a483bf0d0daa8c7906bf1e074
Dam Hong Duc <hongduc001h@gmail.com>
2024-03-18 16:14:57 +0700

    feat: split main and app

---
+++ lib/app.dart
@@ -0,0 +1,67 @@
+import 'package:bot_toast/bot_toast.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:openex_mobile/core/app_router.dart';
+import 'package:openex_mobile/core/common/app_constants.dart';
+import 'package:openex_mobile/core/themes/themes.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:openex_mobile/data/repository/auth_repository.dart';
+import 'package:openex_mobile/features/app/bloc/app_cubit.dart';
+import 'package:openex_mobile/features/auth/bloc/auth_bloc.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiRepositoryProvider(
+        providers: [_configAuthRepository()],
+        child: MultiBlocProvider(
+            providers: [_configAppCubit(), _configAuthBloc()],
+            child: ScreenUtilInit(
+              designSize: const Size(360, 690),
+              minTextAdapt: true,
+              child: BlocBuilder<AppCubit, AppState>(
+                builder: (context, appState) {
+                  return MaterialApp(
+                    localizationsDelegates:
+                        AppLocalizations.localizationsDelegates,
+                    supportedLocales: AppLocalizations.supportedLocales,
+                    locale: Locale(appState.language.currentLang),
+                    builder: BotToastInit(),
+                    navigatorObservers: [BotToastNavigatorObserver()],
+                    title: AppConstants.APP_NAME,
+                    theme: lightTheme,
+                    initialRoute: AppRoute.APP,
+                    routes: appRoutes,
+                    debugShowCheckedModeBanner: false,
+                  );
+                },
+              ),
+            )));
+  }
+
+  RepositoryProvider<AuthRepository> _configAuthRepository() {
+    return RepositoryProvider<AuthRepository>(
+      create: (context) => AuthRepository(),
+      lazy: true,
+    );
+  }
+
+  BlocProvider<AuthBloc> _configAuthBloc() {
+    return BlocProvider<AuthBloc>(
+      create: (context) => AuthBloc(
+        UninitAuthState(),
+        RepositoryProvider.of<AuthRepository>(context),
+      )..add(
+          AuthStartedEvent(),
+        ),
+    );
+  }
+
+  BlocProvider<AppCubit> _configAppCubit() {
+    return BlocProvider<AppCubit>(
+        create: (BuildContext context) => AppCubit()..loadLanguage());
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,6 @@
-import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:openex_mobile/core/app_router.dart';
-import 'package:openex_mobile/core/common/app_constants.dart';
-import 'package:openex_mobile/core/themes/themes.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:openex_mobile/data/repository/auth_repository.dart';
-import 'package:openex_mobile/features/app/bloc/app_cubit.dart';
-import 'package:openex_mobile/features/auth/bloc/auth_bloc.dart';
+import 'package:openex_mobile/app.dart';
 
 void main() {
-  runApp(MultiRepositoryProvider(
-    providers: [
-      RepositoryProvider<AuthRepository>(
-        create: (context) => AuthRepository(),
-        lazy: true,
-      )
-    ],
-    child: MultiBlocProvider(providers: <BlocProvider<dynamic>>[
-      BlocProvider<AppCubit>(
-          create: (BuildContext context) => AppCubit()..loadLanguage()),
-      BlocProvider<AuthBloc>(
-          create: (BuildContext context) => AuthBloc(
-              UninitAuthState(), RepositoryProvider.of<AuthRepository>(context))
-            ..add(AuthStartedEvent()))
-    ], child: const MyApp()),
-  ));
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<AppCubit, AppState>(
-      builder: (context, appState) {
-        return ScreenUtilInit(
-          designSize: const Size(360, 690),
-          minTextAdapt: true,
-          child: MaterialApp(
-            localizationsDelegates: AppLocalizations.localizationsDelegates,
-            supportedLocales: AppLocalizations.supportedLocales,
-            locale: Locale(appState.language.currentLang),
-            builder: BotToastInit(),
-            navigatorObservers: [BotToastNavigatorObserver()],
-            title: AppConstants.APP_NAME,
-            theme: lightTheme,
-            initialRoute: AppRoute.APP,
-            routes: appRoutes,
-            debugShowCheckedModeBanner: false,
-          ),
-        );
-      },
-    );
-  }
+  runApp(const App());
 }
